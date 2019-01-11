86541bb4d1ca1be9b40ea9cc3fe3c51cacc06864
吴丽娟 <wlj@zjmax.com>
2019-01-11 14:37:39 +0800

    新添屏幕录制demo，语言Swift 4.2，日期2019-01-11，创建人Janise。

---
+++ ScreenVideoDemo/ScreenVideoDemo/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  ScreenVideoDemo
+//
+//  Created by Janise on 2019/1/11.
+//  Copyright © 2019年 Janise. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
